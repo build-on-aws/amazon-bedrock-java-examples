0f5999abfe32cb46753051ffe99abb30656f952d
Dennis Traub <dennis.traub@gmail.com>
2023-10-06 11:51:50 +0200

    First example invoking Claude v2
    Co-authored-by: Dennis Traub <traubd@amazon.de>

---
+++ src/main/java/aws/community/examples/InvokeBedrock.java
@@ -0,0 +1,48 @@
+package aws.community.examples;
+
+import java.nio.charset.StandardCharsets;
+
+import org.json.JSONObject;
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
+import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelRequest;
+import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelResponse;
+
+public class InvokeBedrock {
+    public static void main(String[] args) {
+        BedrockRuntimeClient runtime = BedrockRuntimeClient.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+
+        String prompt = "Generate a list of names for a fun brand of chili sauce";
+
+        JSONObject jsonBody = new JSONObject()
+                .put("prompt", "Human: " + prompt + " Assistant:")
+                .put("temperature", 0.8)
+                .put("max_tokens_to_sample", 1024);
+
+        SdkBytes body = SdkBytes.fromUtf8String(
+               jsonBody.toString()
+        );
+
+        InvokeModelRequest request = InvokeModelRequest.builder()
+                .modelId("anthropic.claude-v1")
+                .body(body)
+                .build();
+
+        InvokeModelResponse response = runtime.invokeModel(request);
+
+        JSONObject jsonObject = new JSONObject(
+                response.body().asString(StandardCharsets.UTF_8)
+        );
+
+        String completion = jsonObject.getString("completion");
+
+        System.out.println("-------------------------------------");
+        System.out.println(completion);
+    }
+}
